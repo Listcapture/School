0ceee2f6f65263cedc84717fcebe877c9f034979
Listcapture <2056448327@qq.com>
2023-01-29 21:27:30 +0800

    题解

---
+++ 算法题题解/X.cpp
@@ -0,0 +1,36 @@
+    #include<bits/stdc++.h>
+    using namespace std;
+    typedef long long LL;
+    const int N=2e5+10;
+    #define rep(i,a,b) for(int i=a;i<=b;i++)
+    #define dep(i,a,b) for(int i=a;i>=b;i--)
+    #define lowbit(x) (x&-x)
+    const double eps=1e-8;
+    clock_t startTime;
+    double getCurrentTime() {
+        return (double)(clock() - startTime) / CLOCKS_PER_SEC;
+    }
+    bool check(LL n,LL x)
+    {
+        return n/x>=x-1;
+    }
+    void solve()
+    {
+    
+     
+
+    }
+
+    int main()
+    {
+        int tt;
+        cin>>tt;
+        startTime = clock();
+        while(tt--)
+        {
+            solve();
+            
+
+            //printf("%.2lf",getCurrentTime());
+        }
+    }
