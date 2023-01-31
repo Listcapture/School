4ef2a33ab35aa88b56b6d8eff3ba94de1c79cc93
Listcapture <2056448327@qq.com>
2023-01-31 10:22:56 +0800

    20230131html学习告一段落

--- 算法题题解/X.cpp
+++ 算法题题解/X.cpp
@@ -1,36 +1,39 @@
-    #include<bits/stdc++.h>
-    using namespace std;
-    typedef long long LL;
-    const int N=2e5+10;
-    #define rep(i,a,b) for(int i=a;i<=b;i++)
-    #define dep(i,a,b) for(int i=a;i>=b;i--)
-    #define lowbit(x) (x&-x)
-    const double eps=1e-8;
-    clock_t startTime;
-    double getCurrentTime() {
-        return (double)(clock() - startTime) / CLOCKS_PER_SEC;
-    }
-    bool check(LL n,LL x)
-    {
-        return n/x>=x-1;
-    }
-    void solve()
-    {
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+const int N=2e5+10;
+#define rep(i,a,b) for(int i=a;i<=b;i++)
+#define dep(i,a,b) for(int i=a;i>=b;i--)
+#define lowbit(x) (x&-x)
+const double eps=1e-8;
+clock_t startTime;
+double getCurrentTime() {
+    return (double)(clock() - startTime) / CLOCKS_PER_SEC;
+}
+LL w[N],v[N];
+void solve()
+{
+  int n,m;
+  cin>>n>>m;
+  for(int i=1;i<=n;i++)
+  {
+    cin>>w[i]>>v[i];
+  }
+
     
-     
 
-    }
+}
 
-    int main()
+int main()
+{
+    int tt;
+    tt=1;
+    startTime = clock();
+    while(tt--)
     {
-        int tt;
-        cin>>tt;
-        startTime = clock();
-        while(tt--)
-        {
-            solve();
-            
+        solve();
+        
 
-            //printf("%.2lf",getCurrentTime());
-        }
+        //printf("%.2lf",getCurrentTime());
     }
+}
