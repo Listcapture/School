bc6b82e3fd1349668570569da461815711c402f8
Listcapture <2056448327@qq.com>
2023-02-06 22:18:27 +0800

    20230206树状数组

--- 算法题题解/X.cpp
+++ 算法题题解/X.cpp
@@ -1,64 +1,72 @@
 #include<bits/stdc++.h>
 using namespace std;
-typedef long long LL;
-const int N=2e5+10;
+#define LL long long 
 #define PLL pair<LL,LL>
+#define PII pair<int,int >
+#define fir first
+#define sec second
 #define rep(i,a,b) for(int i=a;i<=b;i++)
 #define dep(i,a,b) for(int i=a;i>=b;i--)
 #define lowbit(x) (x&-x)
 const double eps=1e-8;
-bool cmp(PLL a,PLL b)
-{
-  return a.first<b.first;
-}
 clock_t startTime;
 double getCurrentTime() {
     return (double)(clock() - startTime) / CLOCKS_PER_SEC;
 }
-queue<int >q[30];
-LL n,k;
-string s;
+const int N=3e4+10;
+int n,m;
+LL a[N],tr1[N],tr2[N];
+PLL p[N];
+LL my=0,mx=0;
+int ans[N];
 
-bool cmp(char a,char b)
+void add(LL x,LL d,int status)
 {
-  return a>b;
+  if(status==0) while(x<=mx+1) tr1[x]+=d,x+=lowbit(x);
+  else while(x<=my) tr2[x]+=d,x+=lowbit(x);
+}
+LL query(LL r,int status)
+{
+
+   LL res=0;
+   if(status==0){ while(r) res+=tr1[r],r-=lowbit(r);}
+   else    {while(r) res+=tr2[r],r-=lowbit(r); }
+
+   return res;
+}
+bool cmp(PLL a,PLL b)
+{
+    if(a.fir!=b.fir) return a.fir<b.fir;
+    return a.sec<b.sec;
 }
 void solve()
 {
-   cin>>n>>k>>s;
-   string head,tail;
-   for(int i=0;s[i];i++)
+   scanf("%d",&n);
+   my=0;
+   rep(i,1,n)
    {
-      while(head.size()&&head.size()<s[i]&&k)
-      { 
-        tail.push_back(head.back());
-        head.pop_back();
-        k--;
-      }
-      head.push_bacK(s[i]);
+      scanf("%lld%lld",&p[i].fir,&p[i].sec);
+      p[i].fir=p[i].fir+1,p[i].sec=p[i].sec+1;
+      my=max(my,p[i].sec);
    }
-   while(k&&head.size())
+  // cout<<mx<<" "<<my<<endl;
+   sort(p+1,p+1+n,cmp);
+   rep(i,1,n)
    {
-    tail.push_back(head.back());
-    head.pop_back;
-    k--;
+     ans[query(p[i].sec,1)]++;
+     add(p[i].sec,1,1);
    }
-   sort(tail.begin(),tail.end(),greater());
-   cout<<head+tail<<endl;
-
- 
+   rep(i,0,n-1) printf("%lld\n",ans[i]);
 }
-//ddcbaba ccaa k=2
-//cadbcadbc
 int main()
 {
     int tt;
     tt=1;
-    init();
     startTime = clock();
     while(tt--)
     {
         solve();
         //printf("%.2lf",getCurrentTime());
     }
+    return 0;
 }
---
+++ 算法题题解/tempCodeRunnerFile.cpp
@@ -0,0 +1,2 @@
+x]+=d,x+=lowbit(x);
+  else while(x<=mx) tr2[x]+=d,x+=
