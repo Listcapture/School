02ec8c9398aee0e671a690bc2bed055799c403fe
Listcapture <2056448327@qq.com>
2023-02-07 13:33:14 +0800

    20230207树状数组++

--- 算法题题解/X.cpp
+++ 算法题题解/X.cpp
@@ -13,50 +13,52 @@ clock_t startTime;
 double getCurrentTime() {
     return (double)(clock() - startTime) / CLOCKS_PER_SEC;
 }
-const int N=3e4+10;
+const int N=1e6+10;
 int n,m;
-LL a[N],tr1[N],tr2[N];
+LL mx,my;
+LL tr1[N],tr2[N];
+LL a[N];
 PLL p[N];
-LL my=0,mx=0;
-int ans[N];
-
+LL ans[N];
+bool cmp(PLL a,PLL b)
+{
+  if(a.fir!=b.fir) return a.fir<b.fir;
+  return a.sec<b.sec;
+}
 void add(LL x,LL d,int status)
 {
-  if(status==0) while(x<=mx+1) tr1[x]+=d,x+=lowbit(x);
-  else while(x<=my) tr2[x]+=d,x+=lowbit(x);
+   while(x<=mx) tr1[x]+=d,x+=lowbit(x);
 }
 LL query(LL r,int status)
 {
 
-   LL res=0;
-   if(status==0){ while(r) res+=tr1[r],r-=lowbit(r);}
-   else    {while(r) res+=tr2[r],r-=lowbit(r); }
-
-   return res;
-}
-bool cmp(PLL a,PLL b)
-{
-    if(a.fir!=b.fir) return a.fir<b.fir;
-    return a.sec<b.sec;
+  LL res=0;
+  while(r) res+=tr1[r],r-=lowbit(r);
+  return res;
 }
 void solve()
 {
-   scanf("%d",&n);
-   my=0;
-   rep(i,1,n)
-   {
-      scanf("%lld%lld",&p[i].fir,&p[i].sec);
-      p[i].fir=p[i].fir+1,p[i].sec=p[i].sec+1;
-      my=max(my,p[i].sec);
-   }
-  // cout<<mx<<" "<<my<<endl;
-   sort(p+1,p+1+n,cmp);
-   rep(i,1,n)
-   {
-     ans[query(p[i].sec,1)]++;
-     add(p[i].sec,1,1);
-   }
-   rep(i,0,n-1) printf("%lld\n",ans[i]);
+    cin>>n;
+    for(int i=1;i<=n;i++)cin>>a[i],a[i]++,mx=max(mx,a[i]);
+    for(int i=1;i<=n;i++)
+    { 
+      ans[i]+=query(a[i],0);
+      add(1,1,0),add(a[i],-1,0);
+    }
+    rep(i,1,mx) tr1[i]=0;
+    for(int i=n;i>=1;i--)
+    {
+       ans[i]+=query(a[i],0);
+       add(a[i]+1,1,0);
+    }
+    LL res=0;
+    rep(i,1,n)
+    {
+      
+      res+=ans[i]*(ans[i]+1);
+    }
+    cout<<res/2<<endl;
+    
 }
 int main()
 {
