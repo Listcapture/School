c2cde7faa2cea9b2ae6f00e1ce44b9fe3d80b359
Listcapture <2056448327@qq.com>
2023-02-17 20:45:29 +0800

    20230217

--- 算法题题解/X.cpp
+++ 算法题题解/X.cpp
@@ -14,42 +14,78 @@ double getCurrentTime() {
     return (double)(clock() - startTime) / CLOCKS_PER_SEC;
 }
 const int N=2e5+10;
-LL n;
-
+LL x,y,n,m;
+int e[N],ne[N],h[N],idx;
+int col[N];
+int st[N];
+void add(int a,int b)
+{
+    e[idx]=b,ne[idx]=h[a],h[a]=idx++;
+}
+bool  dfs(int u,int color)
+{
+   if(!col[u]) col[u]=color;
+   for(int i=h[u];~i;i=ne[i])
+   {
+    int j=e[i];
+    if(!col[j])
+    {
+        if(!dfs(j,3^col[u])) return false;
+    }else if(col[j]==col[u]) return false;
+   }
+   return true;
+}
 
 
 void solve()
 {
-    cin>>n;
-    LL k=sqrt(n);
-    rep(a,0,k)
+    while(cin>>x>>y>>n>>m)
+   {
+
+    rep(i,1,x) h[i]=-1,st[i]=0,col[i]=0; idx=0;
+    while(y--)
     {
-      LL s1=a*a;
-      rep(b,0,k)
-      {
-        LL s2=s1+b*b;
-        rep(c,0,k)
-        {
-          LL s3=s2+c*c;
-          rep(d,k,0)
-          {
-            LL s4=s3+d*d;
-            if(s4==n)
-            {
-              cout<<a<<" "<<b<<" "<<c<<" "<<d<<endl;
-              return ;
-            }
-          }
-        }
-      }
+        int a,b;
+        cin>>a>>b;
+        add(a,b),add(b,a);
     }
-    
+    int t;
+    bool can=true;
+    rep(i,1,n) 
+    {
+        cin>>t;
+        col[t]=1;
+        if(!dfs(t,1)) can=false;
+    }
+    rep(i,1,m)
+    {
+        cin>>t;
+        col[t]=2;
+        if(!dfs(t,2)) can=false;
+    }
+    rep(i,1,x) 
+    {
+        if(h[i]==-1)continue;
+        if(!col[i])
+        {
+            if(!dfs(i,1)) 
+            {
+                can=false;
+                break;
+            }
+        }
+    }
+    rep(i,1,x) if(!col[i])can=false;
+    cout<<((can)?"YES":"NO")<<endl;
+   }
+            
+
 }
+
 int main()
 {
     int tt;
     tt=1;
-
     startTime = clock();
     while(tt--)
     {
